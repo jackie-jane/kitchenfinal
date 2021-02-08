3fcc6f81c379bd505af1e35318b92b88adcd57de
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-02-08 17:30:47 -0500

    emoji is now fixed to center of screen and has animation for when it resizes."

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
 import Body from './Components/Layout/Body/Body'
-import QuadrantA from './Components/Quadrants/QuadrantA'
-import QuadrantB from './Components/Quadrants/QuadrantB'
 import Emoji from './Components/Emoji/Emoji'
+import QuadrentA from './Components/Quadrants/QuadrantA'
 import './App.css'
-import Test from './Components/Emoji/Test'
 function App() {
   return (
     <>
-      <Test>
-        <Emoji />
-      </Test>
+      <Body/>
+      <Emoji />
+      <QuadrentA/>
     </>
   )
 }
--- src/Components/Layout/Body/Body.js
+++ src/Components/Layout/Body/Body.js
@@ -1,61 +1,57 @@
 import React, { Component } from 'react';
 import './Body.css'
 
-class Body extends Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {
-    time: 1,
-    keyArray: [],
-    renderArray: [],
-  }
-  componentDidMount() {
-    const list = Array.from(Array(1000).keys())
-    let keyList = []
-    list.forEach(el => {
-      keyList.push(`1010${el}`)
-    })
-    setInterval(() => { this.handleTime() }, 60000);
-    this.handleIntreval()
-    this.setState({
-      keyArray: keyList
-    })
-  }
+// class Body extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   state = {
+//     time: 1,
+//     keyArray: [],
+//     renderArray: [],
+//   }
+//   componentDidMount() {
+//     const list = Array.from(Array(1000).keys())
+//     let keyList = []
+//     list.forEach(el => {
+//       keyList.push(`1010${el}`)
+//     })
+//     setInterval(() => { this.handleTime() }, 60000);
+//     this.handleIntreval()
+//     this.setState({
+//       keyArray: keyList
+//     })
+//   }
 
-  handleTime = () => {
-    let newTime = this.state.time
-    if (this.state.time < 5) {
-      newTime += 1
-    }
-    this.setState({
-      time: newTime
-    })
-  }
-  handleIntreval = () => {
+//   handleTime = () => {
+//     let newTime = this.state.time
+//     if (this.state.time < 5) {
+//       newTime += 1
+//     }
+//     this.setState({
+//       time: newTime
+//     })
+//   }
+//   handleIntreval = () => {
 
-  }
-  render() {
-    return (
-      <div
-        id='bodyOne'>
+//   }
+//   render() {
+//     return (
+//       <div
+//         id='bodyOne'>
 
-      </div>
-    );
-  }
-}
-export default Body;
-
-// function Body(props) {
-//   return (
-//     <div
-//       id='body'
-//       style={{
-
-//       }}>
-//       {props.children}
-//     </div>
-//   );
+//       </div>
+//     );
+//   }
 // }
 
-// export default Body;
+
+function Body(props) {
+  return (
+    <div
+      id='body'>
+    </div>
+  );
+}
+
+export default Body;
