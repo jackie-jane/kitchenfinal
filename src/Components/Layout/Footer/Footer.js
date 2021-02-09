10b9e8c064624f121c5283f76eff62fbcc67ac5a
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-02-09 15:24:06 -0500

    added scroll trigger from npm package

--- src/Components/Layout/Footer/Footer.js
+++ src/Components/Layout/Footer/Footer.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ScrollTrigger from 'react-scroll-trigger';
 import './Footer.css'
-function Footer(props) {
-  return (
-    <div
-      id='footer'>
-    </div>
-  );
+
+class Footer extends Component {
+  state = {
+    text: false
+  }
+  onEnterViewport() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+    this.setState({
+      text: true
+    });
+  }
+  render() {
+    return (
+      <ScrollTrigger>
+        <div
+          id='footer'>
+        </div>
+      </ScrollTrigger>
+    );
+  }
 }
 
 export default Footer
