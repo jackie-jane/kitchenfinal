088d52fb9a467068b431904d8a63fa015430af9f
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-02-12 00:14:18 -0500

    onclick methods up

--- src/Components/Clicks/Clicks.js
+++ src/Components/Clicks/Clicks.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
-import { pickFile } from '../../Services/onClickMethods'
-import { randomClassBackground } from '../../Services/BackgroundServices'
+import { bodyClick, audioPlay, gifResize, flipGif, additionalMethods } from '../../Services/onClickMethods'
 import RendClick from './rendClick'
+import ReactAudioPlayer from 'react-audio-player'
 
 class Clicks extends Component {
   state = {
     clickArr: [],
+    audio: false,
+    audioFile: ''
   }
+
   componentDidMount() {
     window.addEventListener('click', this.handleClick);
+    setTimeout(() => { this.toggleAudio }, 20000)
   }
+
   handleClick = (e) => {
-    const batches = ['A', 'A', 'A', 'B', 'B', 'C', 'C']
-    let batch = batches[Math.floor(Math.random() * batches.length)]
-    let rc = randomClassBackground()
-    let file = pickFile(batch)
-    let finalArr = this.state.clickArr
-    let imgInfo = {
-      'class': `A${rc}`,
-      'url': `media/${batch}/${file}.gif`,
-      'x': e.pageX,
-      'y': e.pageY,
-      'transform': 'none'
+    let res = bodyClick(e, this.state.clickArr)
+    let rand = Math.random() < 0.5
+    let newArr = []
+    if (rand === true) {
+      let probCalc = additionalMethods()
+      if (probCalc === 1) {
+        newArr = gifResize(bodyClick)
+      } else {
+        newArr = flipGif(bodyClick)
+      }
+      this.setState({
+        clickArr: newArr
+      })
+    } else {
+      this.setState({
+        clickArr: res
+      })
     }
-    finalArr.push(imgInfo)
+  }
+
+  toggleAudio = () => {
+    let x = Math.ceil(Math.random() * 3) * 10000
+    setInterval(() => { this.audioSetup }, x);
+  }
+
+  audioSetup = () => {
+    let res = audioPlay()
     this.setState({
-      clickArr: finalArr
+      audio: res.boolean,
+      audioFile: res.audioFile
     })
   }
+
   render() {
     return (
       <>
@@ -36,140 +57,15 @@ class Clicks extends Component {
             {...el}
           />
         )}
+        {this.state.audio ?
+          <ReactAudioPlayer
+            src={`media/audio/${this.state.audioFile}.mp3`}
+            autoPlay />
+          :
+          null}
       </>
     );
   }
 }
 
 export default Clicks;
-
-
-
-// import React, { Component } from 'react';
-// import { randomInt } from '../../Services/Services'
-// import ReactAudioPlayer from 'react-audio-player'
-// import RendClick from './rendClick'
-// import './Clicks.css'
-// import { randomClassBackground } from '../../Services/BackgroundServices';
-// class Clicks extends Component {
-//   state = {
-//     renderedArray: [],
-//     audio: false,
-//     audioFile: ''
-//   }
-
-//   componentDidMount() {
-//     // let response = Array.from({ length: 500 }, () => Math.ceil(Math.random()))
-//     // let final = []
-//     // let current = ''
-//     // response.forEach(el => {
-//       let batches = ['A', 'A', 'A', 'B', 'B', 'C']
-//       let imgBatch = batches[Math.floor(Math.random() * batches.length)]
-//       let url = ''
-//       if (imgBatch === 'A') {
-//         url = Math.floor(Math.random() * 30)
-//       } else if (imgBatch === 'B') {
-//         url = Math.floor(Math.random() * 43)
-//       } else {
-//         url = Math.floor(Math.random() * 24)
-//       }
-//       let img = {
-//         url: `media/${imgBatch}/${url}.gif`,
-//       }
-//       final.push(img)
-//     })
-//     current = final[0]
-//     final.shift()
-//     this.setState({
-//       userImgArray: final,
-//       currentImage: current,
-//     })
-//   }
-
-//   handleClick = () => {
-//     this.imageUpload()
-//     this.audioPlay()
-//     let num = randomInt(100)
-//     if (num <= 10) {
-//       this.gifResize()
-//     } else if (num < 20 && num > 10) {
-//       this.flipGif()
-//     }
-//   }
-
-//   imageUpload = () => {
-//     let final = []
-//     let rendArr = this.state.renderedArray
-//     let upArr = this.state.userImgArray
-//     let current = this.state.userImgArray[0]
-//     final.push(this.state.imageObject)
-//     upArr.shift()
-//     this.setState({
-//       renderedArray: rendArr.concat(final),
-//       userImgArray: upArr,
-//       currentImage: current
-//     })
-//   }
-
-//   audioPlay = () => {
-//     if (this.state.audio === false) {
-//       let file = randomInt(7)
-//       this.setState({
-//         audio: true,
-//         audioFile: file,
-//         play: false
-//       })
-//     }
-//   }
-
-//   gifResize = () => {
-//     let imgArray = this.state.renderedArray
-//     imgArray.forEach(el => {
-//       let newClass = randomClassBackground()
-//       el.class = newClass
-//     })
-//     this.setState({
-//       renderedArray: imgArray
-//     })
-//   }
-
-//   flipGif = () => {
-//     let imgArray = this.state.renderedArray
-//     imgArray.forEach(el => {
-//       let rotate = randomInt(360)
-//       el.transform = `rotate(${rotate}deg)`
-//     })
-//     this.setState({
-//       renderedArray: imgArray
-//     })
-//   }
-//   onEnded = () => {
-//     this.setState({
-//       audio: false
-//     })
-//   }
-//   render() {
-//     return (
-//       <>
-//         {this.state.audio ?
-//           <ReactAudioPlayer
-//             src={`media/audio/${this.state.audioFile}.mp3`}
-//             autoPlay
-//             onEnded
-//           /> : null}
-//         <div
-//           id='userGeneratedImageContainer'
-//           onMouseMove={this.handleTrack}
-//           onClick={this.handleClick}>
-//           {this.state.renderedArray.map(el =>
-//             <RendClick
-//               {...el}
-//             />
-//           )}
-//         </div>
-//       </>
-//     );
-//   }
-// }
-
-// export default Clicks;
